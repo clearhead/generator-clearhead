b5146e0f4139ac16ad7c49daff35d09382fa4f0d
Tom Fuertes <tomfuertes@gmail.com>
2015-03-13 14:42:56 -0500

    add report logger

---
+++ app/templates/base.js
@@ -0,0 +1,27 @@
+// boilerplate templating, error reporting,
+// report(error||string) ==> logs to ga
+// htmlHereDoc(fn) ==> returns string from multiline comment function
+// tmpl ==> handlebarsish / https://github.com/premasagar/tim
+/*jshint ignore:start*/
+$.extend($.exp<%=idx%>, {
+  report: function(a){
+    var b="http://www.google-analytics.com/collect?",
+    d=(new Date).getTime();try{d=/optimizelyEndUserId=([^;]+)/.exec(
+    document.cookie)[1]}catch(a){}(new Image).src=b+$.map({v:1,tid:
+    "UA-33947856-2",cid:d,t:"event",ec:location.hostname,
+    ea:"exp<%=idx%>",el:a.toString(),z:(new Date).getTime()},
+    function(a,b){return b+"="+encodeURIComponent(a)}).get().join("&")
+  },
+  htmlHeredoc: function(fn) {
+    var html = fn.toString().replace(/[^]+\/\*|[^\S]+\*\/[^]+|\s+(?=<)|\n|\t|\r/g, '').replace(/>\s+/g, '>');
+    return $(html);
+  },
+  tmpl:function(){
+    var a=/{{\s*([a-z0-9_][\\.a-z0-9_]*)\s*}}/gi;return function(b,c){
+    return b.replace(a,function(a,b){for(var d=b.split("."),e=d.length,
+    f=c,g=0;e>g;g++){if(f=f[d[g]],void 0===f)throw"tim: '"+d[g]+
+    "' not found in "+a;if(g===e-1)return f}})}
+  }()
+};
+/*jshint ignore:end*/
+
--- app/templates/global.js
+++ app/templates/global.js
@@ -10,28 +10,10 @@
  *    - assumes {{tbd}}
  *
  * ------------------------------------------*/
+$.exp<%=idx%> = {};
 
-<%= analytics %>
+<%=analytics%> // jshint ignore:line
 
-// boilerplate templating and config functions
-window.exp<%=idx%> = {
-  htmlHeredoc: function(fn) {
-    var html = fn.toString().replace(/[^]+\/\*|[^\S]+\*\/[^]+|\s+(?=<)|\n|\t|\r/g, '').replace(/>\s+/g, '>');
-    return $(html);
-  },
-  /*! Tim (lite) github.com/premasagar/tim
-   * A tiny, secure JavaScript micro-templating script. */
-  tim: function() {
-    var e = /{{\s*([a-z0-9_][\\.a-z0-9_]*)\s*}}/gi;
-    return function(f, g) {
-      return f.replace(e, function(h, i) {
-        for (var c = i.split("."), d = c.length, b = g, a = 0; a < d; a++) {
-          b = b[c[a]];
-          if (b === void 0) throw "tim: '" + c[a] + "' not found in " + h;
-          if (a === d - 1) return b
-        }
-      })
-    }
-  }(),
-};
+<%= base %> // jshint ignore:line
+
 /* _optimizely_evaluate=safe */
