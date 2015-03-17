04ba0648c7399ce479a8b998db742d21634a5edf
Tom Fuertes <tomfuertes@gmail.com>
2015-03-17 12:19:55 -0500

    patch base.js

--- app/templates/base.js
+++ app/templates/base.js
@@ -1,9 +1,13 @@
 // boilerplate templating, error reporting,
-// report(error||string) ==> logs to ga
+// log(string) ==> logs to console
+// report(error||string) ==> event to ga
 // htmlHereDoc(fn) ==> returns string from multiline comment function
 // tmpl ==> handlebarsish / https://github.com/premasagar/tim
 /*jshint ignore:start*/
-$.extend($.exp<%=idx%>, {
+$.extend($['exp<%=idx%>'], {
+  log:function(){try{window.console.log.apply(window.console,
+    [].concat.apply(["exp<%=idx%>:"],arguments)),/chdebug/
+  .test(location.href)}catch(a){}},
   report: function(a){
     var b="http://www.google-analytics.com/collect?",
     d=(new Date).getTime();try{d=/optimizelyEndUserId=([^;]+)/.exec(
@@ -12,9 +16,8 @@ $.extend($.exp<%=idx%>, {
     ea:"exp<%=idx%>",el:a.toString(),z:(new Date).getTime()},
     function(a,b){return b+"="+encodeURIComponent(a)}).join("&")
   },
-  htmlHeredoc: function(fn) {
-    var html = fn.toString().replace(/[^]+\/\*|[^\S]+\*\/[^]+|\s+(?=<)|\n|\t|\r/g, '').replace(/>\s+/g, '>');
-    return $(html);
+  html: function(fn) {
+    return fn.toString().replace(/[^]+\/\*|[^\S]+\*\/[^]+|\s+(?=<)|\n|\t|\r/g, '').replace(/>\s+/g, '>');
   },
   tmpl:function(){
     var a=/{{\s*([a-z0-9_][\\.a-z0-9_]*)\s*}}/gi;return function(b,c){
@@ -22,6 +25,6 @@ $.extend($.exp<%=idx%>, {
     f=c,g=0;e>g;g++){if(f=f[d[g]],void 0===f)throw"tim: '"+d[g]+
     "' not found in "+a;if(g===e-1)return f}})}
   }()
-};
+});
 /*jshint ignore:end*/
 
