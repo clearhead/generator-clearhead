96acd92ef4569c5455fb463cc51f7dbb7a65e08a
Tom Fuertes <tomfuertes@gmail.com>
2015-05-01 15:55:18 -0500

    base.js updates to patch modules

--- app/templates/base.js
+++ app/templates/base.js
@@ -1,30 +1,31 @@
-// boilerplate templating, error reporting,
 // log(string) ==> logs to console
-// report(error||string) ==> event to ga
-// htmlHereDoc(fn) ==> returns string from multiline comment function
-// tmpl ==> handlebarsish / https://github.com/premasagar/tim
-/*jshint ignore:start*/
-$.extend($['exp<%=idx%>'], {
-  log:function(){try{window.console.log.apply(window.console,
-    [].concat.apply(["exp<%=idx%>:"],arguments)),/chdebug/
-  .test(location.href)}catch(a){}},
-  report: function(a){
-    var b="http://www.google-analytics.com/collect?",
-    d=(new Date).getTime();try{d=/optimizelyEndUserId=([^;]+)/.exec(
-    document.cookie)[1]}catch(a){}(new Image).src=b+$.map({v:1,tid:
-    "UA-33947856-2",cid:d,t:"event",ec:location.hostname,
-    ea:"exp<%=idx%>",el:a.toString(),z:(new Date).getTime()},
-    function(a,b){return b+"="+encodeURIComponent(a)}).join("&")
-  },
-  html: function(fn) {
-    return fn.toString().replace(/[^]+\/\*|[^\S]+\*\/[^]+|\s+(?=<)|\n|\t|\r/g, '').replace(/>\s+/g, '>');
-  },
-  tmpl:function(){
-    var a=/{{\s*([a-z0-9_][\\.a-z0-9_]*)\s*}}/gi;return function(b,c){
-    return b.replace(a,function(a,b){for(var d=b.split("."),e=d.length,
-    f=c,g=0;e>g;g++){if(f=f[d[g]],void 0===f)throw"tim: '"+d[g]+
-    "' not found in "+a;if(g===e-1)return f}})}
-  }()
-});
-/*jshint ignore:end*/
+$['exp<%=idx%>'].log = function log() { try {
+  window.optimizely = window.optimizely || [];
+  var args = [].concat.apply(["Clearhead", "exp<%=idx%>"], arguments);
+  optimizely.push(args.join(' / '));
+  if (/clearhead-debug=true/.test(document.cookie)) {
+    console.info.apply(console, args);
+  }
+} catch (a) {} };
 
+// report(error||string) ==> event to ga
+$['exp<%=idx%>'].report = function report() {
+  try { var b = "https://ssl.google-analytics.com/collect?", d = (new
+  Date()) .getTime(); try { d = /optimizelyEndUserId=([^;]+)/.exec(
+  document .cookie)[1]; } catch (e) {} d = d.replace(/[^\d]/g, '');
+  (new Image()) .src = b + ($.map({ v: 1, tid: "UA-33947856-2", cid:
+  d.toString(), t: "event", ec: location.hostname, ea: "exp<%=idx%>",
+  el: err.toString(), z: (new Date()).getTime() }, function(a, b) {
+  return b + "=" + encodeURIComponent(a); }).join("&")); } catch (e) {}
+};
+
+// tmpl ==> handlebarsish / https://github.com/premasagar/tim
+(function(a){"use strict";function h(a){var c=b.exec(a.toString());if(
+!c)throw new TypeError("Multiline comment missing.");return c[1];}
+function i(a,b){return a.replace(f,function(a,c){for(var d=c.split("."),
+e=d.length,f=b,h=0;e>h;h++){if(f=f[d[h]],f===g)throw'tim: "'+d[h]+
+'" not found in '+a;if(h===e-1)return f;}});}a.timpl=function(a,b){
+return i(a.call?h(a):a,b||{}).replace(/^\s+|\s+$/g,"");};var g,b=
+/\/\*!?(?:\@preserve)?[ \t]*(?:\r\n|\n)([\s\S]*?)(?:\r\n|\n)[ \t]*\*\//,
+c="{{",d="}}",e="[a-z0-9_$][\\.a-z0-9_]*",f=new RegExp(c+"\\s*("+e+
+")\\s*"+d,"gi");})($['exp<%=idx%>']);
