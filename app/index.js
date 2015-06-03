49d7e3b4be5a3dff0cde1408c1e03c9702fa7d6b
Tom Fuertes <tomfuertes@gmail.com>
2015-06-03 17:19:26 -0500

    fetch from registry

--- app/index.js
+++ app/index.js
@@ -4,24 +4,43 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 var UglifyJS = require('uglify-js');
-
-var updateNotifier = require('update-notifier');
-var pkg = require('../package.json');
-var notify = updateNotifier({pkg: pkg});
-
+var request = require('request');
 var conf;
-module.exports = yeoman.generators.Base.extend({
 
-  initializing: function () {
-    this.pkg = require('../package.json');
+module.exports = yeoman.generators.Base.extend({
+  constructor: function() {
+    yeoman.generators.Base.apply(this, arguments);
+    this.option('skip-update');
+    this.skipUpdate = (this.options['skip-update'] ? true : false);
   },
 
-  prompting: function () {
+  initializing: function() {
+
+    this.pkg = require('../package.json');
+
+    var done = this.async();
+    request('https://registry.npmjs.org/generator-clearhead', function(e, r, body) {
+      var edge = false;
+      try {
+        edge = (JSON.parse(body)['dist-tags'].latest === this.pkg.version);
+      } catch (e) {}
+      if (this.skipUpdate || edge) {
+        done();
+      } else {
+        this.log(yosay(
+          'Please update using `npm update -g generator-clearhead`' +
+          ' or run again using the `--skip-install` flag'
+        ));
+      }
+    }.bind(this));
+  },
+
+  prompting: function() {
     var done = this.async();
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the cat\'s pajamas ' + chalk.red('Clearhead') + ' generator!'
+      'Welcome to the ' + chalk.red('Clearhead') + ' generator!'
     ));
 
     var idx;
@@ -29,45 +48,43 @@ module.exports = yeoman.generators.Base.extend({
       type: 'input',
       name: 'idx',
       message: 'exp-{{int}}-slug',
-      validate: function (input) {
+      validate: function(input) {
         return /^\d+$/.test(idx = input);
       }
-    },{
+    }, {
       type: 'input',
       name: 'name',
       message: function() {
-        return 'exp-'+idx+'-{{slug}}';
+        return 'exp-' + idx + '-{{slug}}';
       },
-      validate: function (input) {
+      validate: function(input) {
         return /^[a-z0-9-]+$/.test(input);
       }
-    },{
+    }, {
       type: 'input',
       name: 'plan',
       message: 'Test Plan Link? (optional)'
-    },{
+    }, {
       type: 'list',
       name: 'analytics',
       message: 'Analytics?',
       choices: getFiles(__dirname + '/templates/analytics/'),
-      validate: function (input) {
+      validate: function(input) {
         return !!input;
       }
-    },{
+    }, {
       type: 'input',
       name: 'author',
       message: 'Author?',
-      validate: function (input) {
+      validate: function(input) {
         return !!input;
       }
     }];
 
-    this.prompt(prompts, function (props) {
-
-      notify.notify();
+    this.prompt(prompts, function(props) {
 
       conf = {
-        name: 'exp-'+props.idx+'-'+props.name,
+        name: 'exp-' + props.idx + '-' + props.name,
         idx: props.idx,
         plan: props.plan,
         author: props.author,
@@ -81,16 +98,22 @@ module.exports = yeoman.generators.Base.extend({
   },
 
   writing: {
-    globals: function () {
+    globals: function() {
       conf.base = fs.readFileSync(conf.baseFileName).toString();
       conf.base = this.engine(conf.base, conf); // compile base
-      conf.base = UglifyJS.minify(conf.base, { fromString: true,
-        output: {comments:true} }).code;
+      conf.base = UglifyJS.minify(conf.base, {
+        fromString: true,
+        output: {
+          comments: true
+        }
+      }).code;
       var analyticsName = conf.analytics;
       var analytics = fs.readFileSync(__dirname + '/templates/analytics/' + analyticsName).toString();
       analytics = this.engine(analytics, conf);
-      analytics = UglifyJS.minify(analytics, { fromString: true,
-        /*output: {comments:true}*/ }).code;
+      analytics = UglifyJS.minify(analytics, {
+        fromString: true,
+        /*output: {comments:true}*/
+      }).code;
       analytics = '// ' + analyticsName + '\n' + analytics;
       conf.analytics = analytics;
       this.fs.copyTpl(
@@ -105,7 +128,7 @@ module.exports = yeoman.generators.Base.extend({
       );
     },
 
-    variations: function () {
+    variations: function() {
       this.fs.copyTpl(
         this.templatePath('control.js'),
         this.destinationPath(this.slug + 'control.js'),
@@ -119,15 +142,15 @@ module.exports = yeoman.generators.Base.extend({
     }
   },
 
-  install: function () {
+  install: function() {
     this.installDependencies({
       skipInstall: true
     });
   },
 
-  end: function () {
+  end: function() {
     this.log(yosay(
-      'Thanks! ctrl+f TODO if you included any analytics.'
+      'Generated!'
     ));
   }
 });
